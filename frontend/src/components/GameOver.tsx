fca63fff6f5b06858fe79825ac267a1664aaeb0f
gli-i <133687652+gli-i@users.noreply.github.com>
2023-11-08 17:30:41 -0800

    implement timed responsive design + basic scoring

--- frontend/src/components/GameOver.tsx
+++ frontend/src/components/GameOver.tsx
@@ -1,18 +1,28 @@
 interface GOProps {
     restartGame: () => void;
+    score:number;
 }
 
-export default function GameOver({ restartGame }:GOProps){
+export default function GameOver({ restartGame, score }:GOProps){
 
     return (
         <>
-        <div className="z-20 fixed inset-x-1/4 top-1/4 bg-gray-600 p-4
+        <ul className="z-20 fixed inset-x-1/4 md:inset-x-1/3 top-1/4 bg-white p-4
             flex flex-col justify-center items-center gap-3
+            md:text-lg lg:text-xl
         ">
-            <h3>Game Over!</h3>
-            <button type="button" onClick={restartGame} className="bg-white rounded p-1">Restart</button>
-        </div>
-        <div className="z-10 fixed w-full h-[100vh] bg-black opacity-40"></div>
+            <li>Game Over!</li>
+            <li className="flex flex-col items-center">
+                <p>Your score was: </p>
+                <p className="font-semibold">{score}</p>
+            </li>
+            <li className="flex flex-col items-center">
+                <p>Top Score: </p>
+                <p className="font-semibold">{score}</p>
+            </li>
+            <button type="button" onClick={restartGame} className="bg-gray-300 border border-gray-500 rounded p-1">Restart</button>
+        </ul>
+        <div className="z-10 fixed w-full h-[100vh] bg-black opacity-30"></div>
         </>
     )
 }
