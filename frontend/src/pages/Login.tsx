535c726a6bea7a4a6fbd8963577023dd183c50a8
Sam <samwentupp@outlook.com>
2023-11-10 16:12:19 -0800

    login page

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,22 +1,56 @@
-export default function Login(){
+import { useState, useEffect } from "react"  
+import { Link, useNavigate } from "react-router-dom";
+
+
+export default function Login() {
+    const navigate = useNavigate()
+
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+    async function sendLoginRequest(){
+        try {
+            const res = await fetch('http://127.0.0.1:3000/signin', {
+                method: "POST",
+                body: JSON.stringify({_id: username, password: password}),
+                credentials: "include"
+            });
+            const resJson = await res.json();
+
+            if (res.ok){
+                navigate('/')
+            }
+            else{
+                throw new Error("bad request"); 
+            }
+        } catch (error) {
+            console.log(error); 
+        }
+    }
+
+
     return (
-        <> 
-            <header className='flex justify-center align-center p-1 font-bold border-b-2 border-gray-300'>
-            <h1 className='text-xl'>Wordle</h1>
-            </header>
-            <div className='max-w-4xl min-w-fit m-auto'>
-                <div className={'min-h-fit max-h-60 m-24 py-24 px-10 border-2 border-gray-300 rounded  h-screen h-full flex flex-auto flex-col justify-center align-center font-sans'}>
-                    <h1 className={'text-center text-4xl font-bold'} >Log in!</h1> <br></br><br></br><br></br><br></br>
-                    <label className={'text-lg py-0.5'}>Username</label>
-                    <input className={'h-8 border rounded border-gray-500'}type="text" id="signupUsername"/> <br></br>
-                    <label className={'text-lg py-0.5'}>Password</label>
-                    <input className={'h-8 border rounded border-gray-500'}type="text" id="signupPassword"/> <br></br>
-                    <button className={'h-10 border rounded border-black bg-black text-white'} type="button">Log in</button><br></br>
-                    <label className={'text-center text-[18px]'}>Not a member?   
-                        <a href="../pages/Signup.tsx link later" className='text-blue-500'> Sign up! </a>
-                    </label>
+        <div className="w-screen h-screen">
+            <div className="w-full h-5/6 flex justify-center items-center">
+                <div className="w-96 mx-auto shadow-2xl">
+                    <div className="text-center my-2 font-bold text-xl">Wordle</div>
+                    <div className="text-center">Log in to continue</div>
+                    <div className="w-4/5 mx-auto my-2">
+                        <input onChange={(e)=>{setUsername(e.target.value)}} value={username} type="text" className="w-full h-full border-2 border-slate-100 p-2" placeholder="Enter your username" />
+
+                    </div>
+                    <div className="w-4/5 mx-auto mb-2">
+                        <input onChange={(e)=>{setPassword(e.target.value)}} value={password} type="text" className="w-full h-full border-2 border-slate-100 p-2" placeholder="Enter your password" />
+                    </div>
+                    <div className="w-4/5 mx-auto mb-4">
+                        <div onClick={sendLoginRequest} className="w-full h-full bg-blue-800 text-white rounded-sm text-center p-2 cursor-pointer hover:bg-blue-500">Login</div>
+                    </div>
+                    <div className="w-4/5 mx-auto mb-4 flex gap-1 flex-col ">
+                        <Link to={'/sign-up'} className=" text-blue-500 cursor-pointer border-b">Create an account</Link>
+                        <Link to={'/'} className=" text-blue-500 cursor-pointer">Maybe later</Link>
+                    </div>
                 </div>
             </div>
-        </>
+        </div>
     )
 }
