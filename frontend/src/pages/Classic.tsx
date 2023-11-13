ad7b1cd7b289acbb4ea066f7f4459aeefc42a339
Sam <samwentupp@outlook.com>
2023-11-12 16:25:25 -0800

    classic computeScore url change

--- frontend/src/pages/Classic.tsx
+++ frontend/src/pages/Classic.tsx
@@ -68,7 +68,11 @@ export default function Classic() {
       let concatedStr = concatStringArr(words[arrayIndex]);
 
       try {
-        const response = await fetch(`https://2ev2xiv117.execute-api.us-east-1.amazonaws.com/Prod/api/checkWord?word=${concatedStr}`);
+        const response = await fetch(`https://0indrq4mb3.execute-api.us-east-1.amazonaws.com/Prod/computescore`, {
+          method: "POST", 
+          body: JSON.stringify({word: concatedStr}), 
+          credentials: "include" 
+        });
         const jsonRes: wordCheckResponseInterface = await response.json();
 
         if (jsonRes.found) {
